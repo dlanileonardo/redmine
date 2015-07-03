376bcfb53d6f48c08cf8525ace0ac2f3ea9891b9
Dlani Mendes <dlanileonardo@gmail.com>
2015-07-02 22:59:10 -0300

    Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,3 +28,13 @@ config.action_mailer.raise_delivery_errors = false
 
 # No email in production log
 config.action_mailer.logger = nil
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
