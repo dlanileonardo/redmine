d3de6132d7689f4a22f7e1809d0e708789a30e4d
Dlani Mendes <dlanileonardo@gmail.com>
2015-07-03 16:58:46 -0300

    Adjust Plugin

--- vendor/plugins/redmine_git_remote-master/lib/redmine_git_remote/repositories_helper_patch.rb
+++ vendor/plugins/redmine_git_remote-master/lib/redmine_git_remote/repositories_helper_patch.rb
@@ -8,11 +8,11 @@ module RedmineGitRemote
       def git_remote_field_tags(form, repository)
         content_tag('p', form.text_field(:url,
                            :size => 60, :required => true, :required => false,
-                           :disabled => !repository.safe_attribute?('url'),
+                           :disabled => (repository && !repository.new_record?),
                            :label => l(:field_path_to_repository)) +
                           content_tag('em', l(:text_git_remote_path_note), :class => 'info') +
                           form.text_field(:extra_clone_url, :size => 60, :required => true,
-                           :disabled => !repository.safe_attribute?('url')) +
+                           :disabled => (repository && !repository.new_record?)) +
                           content_tag('em', l(:text_git_remote_url_note), :class => 'info')
                    )
       end
